baacb5709ba0ca1b9c58c86efca156078559c127
judycodes <43575215+judycodes@users.noreply.github.com>
2019-10-27 19:21:32 -0400

    view resume toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 //console.log("js is linked!");
+
+//desktop - resume iframe view
+const view_resume = document.getElementById('view_resume_btn');
+
+const resume_pdf = document.getElementById('resume_pdf');
+
+view_resume.addEventListener('click', () => {
+    resume_pdf.classList.toggle('none');
+    if(resume_pdf.classList.contains('none')) {
+        view_resume.innerHTML = "view resume";
+    } else {
+        view_resume.innerHTML = "minimize resume";
+    }
+    
+});
+
