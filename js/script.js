3b797b0ba146a336637fab59033e5a3d424cf40a
judycodes <43575215+judycodes@users.noreply.github.com>
2021-09-30 09:34:37 -0400

    chore: fix read feature and add increadible info

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 //console.log("js is linked!");
 
 //desktop - resume iframe view
-const view_resume = document.getElementById('view_resume_btn');
+// const view_resume = document.getElementById('view_resume_btn');
 
-const resume_pdf = document.getElementById('resume_pdf');
+// const resume_pdf = document.getElementById('resume_pdf');
 
-view_resume.addEventListener('click', () => {
-    resume_pdf.classList.toggle('none');
-    if(resume_pdf.classList.contains('none')) {
-        view_resume.innerHTML = "view resume";
-    } else {
-        view_resume.innerHTML = "minimize resume";
-    }
+// view_resume.addEventListener('click', () => {
+//     resume_pdf.classList.toggle('none');
+//     if(resume_pdf.classList.contains('none')) {
+//         view_resume.innerHTML = "view resume";
+//     } else {
+//         view_resume.innerHTML = "minimize resume";
+//     }
     
-});
+// });
 
 //desktop - open project content
 let project_read_btns = document.querySelectorAll('.project_read');
